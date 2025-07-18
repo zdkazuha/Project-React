3e2dcd9b9c234ceb7204483ef0900fd34da17ba0
zdkazuha <aartemchelyk@gmail.com>
2025-07-18 18:13:55 +0300

    update

--- cinema-project/src/components/Home.jsx
+++ cinema-project/src/components/Home.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import img from '../img/canvas.png'
 import MovieCard from './MovieCard'
-import { useMessage } from '../hooks/useMessage';
 
 export default function Home() {
     const api = 'https://api.themoviedb.org/3/movie/popular?api_key=b507e73c6fb26fa0dcacca602b38a41e&language=en-US&page=1';
 
     const [movies, setMovies] = useState([]);
-    const { contextHolder, showSuccess } = useMessage();
 
     useEffect(() => {
         fetchMovies();
